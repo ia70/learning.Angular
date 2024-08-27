26769b648a30f51bafe3315cd8d12e13c28ddc48
Daniel Perez Cortez <pcd510@hotmail.com>
2024-08-27 06:03:35 -0600

    Fundamentos de Angular

--- mi-primera-app/src/app/app.component.ts
+++ mi-primera-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'mi-primera-app';
+  titulo = 'Primera modificación en Angular';
 }
---
+++ mi-primera-app/src/app/personas/personas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-personas',
+    templateUrl: './personas.component.html',
+    styleUrl: './personas.component.css'
+})
+export class PersonasComponent{
+
+}
