3baab588f09ca5e4879b84a3c0eac5ed0948b724
Daniel Perez Cortez <pcd510@hotmail.com>
2024-08-30 09:43:16 -0600

    Directiva *ngFor

---
+++ listado-personas/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Persona } from './persona.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  titulo = 'Listado de Personas';
+  nombreInput: string ='';
+  apellidoInput: string ='';
+
+  personas: Persona[] = [
+    new Persona("Juan","Pérez"), 
+    new Persona("Laura","Juarez"),
+    new Persona("Carla", "Lara")
+  ];
+
+  agregarPersona(){
+    this.personas.push(new Persona(this.nombreInput, this.apellidoInput));
+  }
+
+}
