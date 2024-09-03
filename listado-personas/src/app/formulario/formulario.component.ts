f5d83d6af60721ff979193f44efe50b676652f07
Daniel Perez Cortez <pcd510@hotmail.com>
2024-09-02 18:52:16 -0600

    Servicios en Angular

--- listado-personas/src/app/formulario/formulario.component.ts
+++ listado-personas/src/app/formulario/formulario.component.ts
@@ -1,21 +1,31 @@
 import {Component, ElementRef, EventEmitter, Output, ViewChild,} from '@angular/core';
 import { Persona } from '../persona.model';
+import { LoggingService } from '../LoggingService.service';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css',
+  providers: [LoggingService]
 })
 export class FormularioComponent {
   @Output() personaCreada = new EventEmitter<Persona>();
   @ViewChild('nombreInput') nombreInput: ElementRef;
   @ViewChild('apellidoInput') apellidoInput: ElementRef;
 
+  constructor(private loggingService: LoggingService){}
+
   agregarPersona() {
-    this.personaCreada.emit(
-      new Persona(
-        this.nombreInput.nativeElement.value,
-        this.apellidoInput.nativeElement.value)
+    let persona: Persona;
+
+    persona = new Persona(
+      this.nombreInput.nativeElement.value,
+      this.apellidoInput.nativeElement.value);
+
+    this.loggingService.enviaMensajeAConsola(
+      "Enviamos persona: " + persona.nombre + " " + persona.apellido
     );
+
+    this.personaCreada.emit(persona);
   }
 }
