d68b1ff74116df0eca3b1d2c8144629b8e822b79
Daniel Perez Cortez <pcd510@hotmail.com>
2024-09-21 00:16:23 -0600

    pipes

--- Curso-02/movies-webapp/src/app/app.component.ts
+++ Curso-02/movies-webapp/src/app/app.component.ts
@@ -1,12 +1,34 @@
+import {
+  CurrencyPipe,
+  DatePipe,
+  DecimalPipe,
+  JsonPipe,
+  LowerCasePipe,
+  PercentPipe,
+  SlicePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    DatePipe,
+    CurrencyPipe,
+    LowerCasePipe,
+    UpperCasePipe,
+    PercentPipe,
+    DecimalPipe,
+    TitleCasePipe,
+    JsonPipe,
+    SlicePipe
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'movies-webapp';
