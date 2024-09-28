13f93320f2a002e05ceff6e8ff32e393383b607c
Daniel Perez Cortez <pcd510@hotmail.com>
2024-09-28 00:18:17 -0600

    pipes

--- Curso-02/movies-webapp/src/app/app.component.ts
+++ Curso-02/movies-webapp/src/app/app.component.ts
@@ -32,4 +32,29 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'movies-webapp';
+  
+  peliculas = [
+    {
+      titulo: 'Spider-Man',
+      fechaLanzamiento: new Date(),
+      precio: 1400.99
+    },
+    {
+      titulo: 'Moana',
+      fechaLanzamiento: new Date(),
+      precio: 1350.99
+    },
+    {
+      titulo: 'Ridick',
+      fechaLanzamiento: new Date(),
+      precio: 899.30
+    },
+    {
+      titulo: 'I am leyent 2',
+      fechaLanzamiento: new Date(),
+      precio: 1680.50
+    }
+  ]
+
+
 }
